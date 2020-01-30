9b50b20549f3a753c914a55bd9ff484aac105d76
Dizzudin2 <36229216+Dizzudin2@users.noreply.github.com>
2020-01-30 14:48:32 +0800

    close #1 (#4)
    * Adding peering module
    
    * Add support for different resource group vnet peering.
    
    * update variables.tf
    
    * update main.tf
    
    * Update README.md

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "vnet_resource_group_name_1" {
+  description = "Resource group name for first virtual network."
+}
+
+variable "vnet_resource_group_name_2" {
+  description = "Resource group name for second virtual network."
+}
+
+variable "vnet_name_1" {
+  description = "The name of the first virtual network."
+}
+
+variable "vnet_name_2" {
+  description = "The name of the second virtual network."
+}
